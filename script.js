bf79082c274dddeeff1c8a67e9cff8336e7c0cd8
Hassan Shah <syedhassaanalishah105@gmail.com>
2025-12-06 22:05:55 +0500

    big update
    big update

---
+++ script.js
@@ -0,0 +1,160 @@
+// // chapter 9 to 13 task 1
+// var cityName = prompt("Enter city name:");
+// if (cityName === "karachi") {
+//     alert("Welcome to the city of lights");
+
+// }
+
+// // chapter 9 to 13 task 2
+// var gender = prompt("","Enter your gender")
+// if(gender == "male"){
+//     alert("Good Morning Sir.")
+// }
+//  else if(gender == "female"){
+//     alert("Good Morning Ma'am.")
+    
+// }else{
+//     alert("please type your corect gender in lowercas")
+// }
+
+// // chapter 9 to 13 task 3
+// var color = prompt("","Enter your signal color in lower case")
+// if(color == "red"){
+//     alert("Must Stop")
+// }
+//  else if(color == "yellow"){
+//     alert("Ready to move")
+//  }
+//  else if(color == "green"){
+//     alert("Move now")
+//  }else{
+//    alert("Please type correct color in lower case")
+//  }
+
+
+// chapter 9 to 13 task 3
+// var fuel = +prompt("","Enter remaining fuel in car in litres")
+// if(fuel < 0.25){
+//     alert("Please refill the fuel in your car")
+// }
+
+
+// chapter 9 to 13 task 4
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+//  
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");    
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){           
+// alert("condition 3 is true");    
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }        
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }        
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+// var marksObtained = +prompt("","Enter your marks obtained in three subjects")        
+// var totalMarks = +prompt("","Enter total marks")
+// var percentage = (marksObtained / totalMarks) * 100;
+// document.write("Marks Obtained: " + marksObtained + "<br>")
+// document.write("Total Marks: " + totalMarks + "<br>")
+// document.write("Percentage: " + percentage + "%" + "<br>")
+// if (percentage >= 80) {
+//     document.write("Grade: A-one" + "<br>")
+//     document.write("Remarks: Excellent" + "<br>")    
+// } else if (percentage >= 70) {
+//     document.write("Grade: A" + "<br>")
+//     document.write("Remarks: Good" + "<br>") 
+// } else if (percentage >= 60) {
+//     document.write("Grade: B" + "<br>")
+//     document.write("Remarks: You need to improve" + "<br>") 
+// } else {
+//     document.write("Grade: Fail" + "<br>")
+//     document.write("Remarks: Sorry" + "<br>") 
+// }    
+// var secretNumber = 7;
+// var userGuess = +prompt("","Guess the secret number between 1 to 10")
+// if (userGuess === secretNumber) {    
+//     alert("Bingo! Correct answer")
+// } else if (userGuess === secretNumber + 1) {
+//     alert("Close enough to the correct answer")          
+// } else {
+//     alert("Try again!")          
+// }
+// var number = +prompt("","Enter a number to check whether it is divisible by 3 or not")
+// if (number % 3 === 0) {
+//     alert(number + " is divisible by 3") 
+// } else {
+//     alert(number + " is not divisible by 3") 
+// }    
+// var number = +prompt("","Enter a number to check whether it is even or odd")
+// if (number % 2 === 0) {
+//     alert(number + " is even number")    
+// } else {
+//     alert(number + " is odd number")    
+// }    
+
+// var temperature = +prompt("","Enter temperature")        
+// if (temperature > 40) {
+//     alert("It is too hot outside.")    
+// } else if (temperature > 30) {   
+//     alert("The Weather today is Normal.")    
+// } else if (temperature > 20) {
+//     alert("Today’s Weather is cool.")
+// } else if (temperature > 10) {
+//     alert("OMG! Today’s weather is so Cool.")
+// }
+// var firstNumber = +prompt("","Enter first number")
+// var secondNumber = +prompt("","Enter second number")
+// var operation = prompt("","Enter operation (+, -, *, /, %)")
+// var result;
+// if (operation === "+") {
+//     result = firstNumber + secondNumber;
+//     alert("The sum of " + firstNumber + " and " + secondNumber + " is " + result)    
+// } else if (operation === "-") {
+//     result = firstNumber - secondNumber;
+//     alert("The difference of " + firstNumber + " and " + secondNumber + " is " + result)
+// } else if (operation === "*") {
+//     result = firstNumber * secondNumber;
+//     alert("The product of " + firstNumber + " and " + secondNumber + " is " + result)
+// } else if (operation === "/") {
+//     result = firstNumber / secondNumber;
+//     alert("The division of " + firstNumber + " by " + secondNumber + " is " + result)
+// } else if (operation === "%") {
+//     result = firstNumber % secondNumber;
+//     alert("The modulus of " + firstNumber + " and " + secondNumber + " is " + result)
+// } else {
+//     alert("Invalid operation")
+// }    
+
+
+
+
+
+
+
